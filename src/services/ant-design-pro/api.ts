c76765d5bb1540b1f4cf994ba295ff6eb6481b6d
梁健辉 <ljk15916807596@qq.com>
2023-02-22 11:38:35 +0800

    fix: 本地代理修复

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -1,10 +1,11 @@
 // @ts-ignore
 /* eslint-disable */
 import { request } from 'umi';
+import config from '../../config';
 
 /** 获取当前的用户 GET /api/currentUser */
 export async function currentUser(options?: { [key: string]: any }) {
-  return request<any>('/modelDev/user/me1', {
+  return request<any>(`${config.basePath}/user/me1`, {
     method: 'GET',
     ...(options || {}),
   });
