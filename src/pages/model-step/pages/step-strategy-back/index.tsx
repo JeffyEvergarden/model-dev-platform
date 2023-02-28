1b9a9ccbe434b6f11725c4d0c258d60327a95a9c
13113193306 <jiahao3306@163.com>
2023-02-28 09:36:33 +0800

    fix: 34

--- src/pages/model-step/pages/step-strategy-back/index.tsx
+++ src/pages/model-step/pages/step-strategy-back/index.tsx
@@ -5,6 +5,8 @@ import Condition from '@/components/Condition';
 import TabOne from './tab-one';
 import TabTwo from './tab-two';
 import TitleStatus from '../../components/title-status';
+import { useModel } from 'umi';
+import { getWaitResult } from '../step-select-sample/model/api';
 
 // 首页
 const StepStrategyBack: React.FC<any> = (props: any) => {
@@ -16,6 +18,11 @@ const StepStrategyBack: React.FC<any> = (props: any) => {
 
   const [selectedKeys, setSelectedKeys] = useState<any[]>([]);
 
+  const { modelId, doneStep } = useModel('step', (model: any) => ({
+    modelId: model.modelId,
+    doneStep: model.doneStep,
+  }));
+
   // 过程id
   const [processId, setProcessId] = useState<any>('000');
 
@@ -26,6 +33,34 @@ const StepStrategyBack: React.FC<any> = (props: any) => {
 
   const again = () => {
     setStepType(1);
+  };
+
+  useEffect(() => {
+    {
+      /*进入页面调用状态接口判断该步骤状态
+        页面是否已提交，仅仅适用于有loading页面的阶段。
+        当前阶段状态：0：未开始 1：进行中 2：已完成 3：处理失败
+        因部分阶段有loading页面，所以需要给标识，区分是进入loading页面还是参数选择页面的。
+        只有（当前阶段状态-进行中+isCommittedPage=true）才会进入loading页面。
+
+        当前阶段状态 1 &isCommittedPage=true --进入loading页面 loading 状态
+        当前阶段状态 2 --进入--进入loading页面 完成 状态
+        当前阶段状态 0 3 --进入--进入loading页面 处理失败 状态
+  */
+    }
+    getCurrentStage();
+  }, []);
+
+  const getCurrentStage = async () => {
+    let res = await getWaitResult({ itmModelRegisCode: modelId });
+    let data = res.result || {};
+    if (data.currentStageStatus == '2' || data.currentStageStatus == '3') {
+      setStepType(2);
+    } else if (data?.currentStageStatus == '1' && data?.isCommittedPage == '1') {
+      setStepType(2);
+    } else {
+      setStepType(1);
+    }
   };
 
   return (
